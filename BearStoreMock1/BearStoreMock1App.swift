3459124401b6f2ff191e4673513ba3620fa234db
Eric Cheung <eric1162004@hotmail.com>
2022-04-01 11:18:07 -0700

    Initial Commit

---
+++ BearStoreMock1/BearStoreMock1App.swift
@@ -0,0 +1,17 @@
+//
+//  BearStoreMock1App.swift
+//  BearStoreMock1
+//
+//  Created by eric on 2022-04-01.
+//
+
+import SwiftUI
+
+@main
+struct BearStoreMock1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
